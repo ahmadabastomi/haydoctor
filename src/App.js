b41bfeebad50dc7510353bc9a5e16ed440f1ed9c
ahmadabastomi <ahmadabastomi@gmail.com>
2021-01-22 10:07:03 +0700

    fix react native import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { GetStarted } from './pages';
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import 'react-native-gesture-handler';
 import Router from './router';
 
 const App = () => {
